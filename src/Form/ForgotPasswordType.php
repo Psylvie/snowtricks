e1839c1c352ac7055349a0611c878e5eac13d337
Psylvie <peuzin.sylvie.sp@gmail.com>
2024-07-31 19:06:26 +0200

    bug fix on password reset

--- src/Form/ForgotPasswordType.php
+++ src/Form/ForgotPasswordType.php
@@ -3,7 +3,6 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -25,7 +24,7 @@ class ForgotPasswordType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => 'App\Entity\User',
+            // Configure your form options hereuser
         ]);
     }
 }
